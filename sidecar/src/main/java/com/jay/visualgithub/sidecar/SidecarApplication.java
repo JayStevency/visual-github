5b694c2087eaf6d60590c072c0f110064929c869
jaystevency <yjy1129@kookmin.ac.kr>
2018-05-11 09:51:55 +0900

    added sidecar service

---
+++ sidecar/src/main/java/com/jay/visualgithub/sidecar/SidecarApplication.java
@@ -0,0 +1,16 @@
+package com.jay.visualgithub.sidecar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.sidecar.EnableSidecar;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableSidecar
+public class SidecarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SidecarApplication.class, args);
+	}
+}
