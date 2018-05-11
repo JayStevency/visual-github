b053fdb4ea76340ab87a421d03568c3570cb8614
jaystevency <yjy1129@kookmin.ac.kr>
2018-05-11 09:49:19 +0900

    added github api server

---
+++ github-api/routes/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+const rp = require('request-promise');
+
+/* GET home page. */
+router.get('/health.json', (req, res) => {
+    res.json({
+        "status" : "UP"
+    });
+});
+
+router.get('/a', (req, res) => {
+    res.send("From Node Server");
+})
+
+router.get('/github/:githubId', (req, res) => {
+    const {githubId} = req.params;
+    const options = {
+        uri: 'https://api.github.com/users/'+githubId+'/repos',
+        headers: {
+            'User-Agent': '*'
+        },
+        json: true // Automatically parses the JSON string in the response
+    };
+    rp(options)
+        .then(result => {
+            res.json(result);
+        }).catch(err => {
+            console.log(err.message);
+            res.json([]);
+    });
+});
+
+module.exports = router;
